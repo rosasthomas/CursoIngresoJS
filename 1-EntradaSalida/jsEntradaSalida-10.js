01882934ccd420a314feea759205bf39c3fbda4f
rosasthomas <rosasthomas2@gmail.com>
2018-07-05 09:55:42 -0300

    Ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,10 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var importe,resultado;
+    importe = document.getElementById("importe").value;
+    importe = parseInt(importe);
+    resultado = importe * 0.25;
+    resultado = importe - resultado;
+    document.getElementById("resultado").value = resultado;
 }
