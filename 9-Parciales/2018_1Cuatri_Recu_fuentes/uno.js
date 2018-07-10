8b188c7512b7d7bb70def5ccef3ad9ea95a9e198
rosasthomas <rosasthomas2@gmail.com>
2018-07-10 09:50:43 -0300

    Arreglos

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -3,6 +3,8 @@ function mostrar()
 {   var base,altura,superficie,perimetro;
     base = prompt("Base");
     altura = prompt("Altura");
+    base = parseInt(base);
+    altura = parseInt(altura);
     superficie = base * altura;
     superficie = superficie / 2;
     perimetro = base * 3;
