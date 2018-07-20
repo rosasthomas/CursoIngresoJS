02fe336924bee1a76a14b1380ef6c45799bb0ea0
rosasthomas <rosasthomas2@gmail.com>
2018-07-20 17:50:25 -0300

    ej 7, incompleto

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -4,26 +4,31 @@ siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
 El jugador seleccionará una imagen correspondiente 
 a su opción  y le informaremos si ganó, empató o perdió.
 */
-var eleccionMaquina;
-
+var eleccionMaquina,maquina;
+document.getElementById("piedra()") = 1;
+document.getElementById("papel()") = 2;
+document.getElementById("tijera()") = 3;
 function comenzar()
 {
-	
-
-
+    eleccionMaquina = parseInt(Math.random() * (4 - 1) + 1);
+    console.log(eleccionMaquina);
+    if(eleccionMaquina == document.getElementById("piedra").value ){
+        maquina = document.getElementById("piedra").value;
+    }else if(eleccionMaquina == document.getElementById("papel").value){
+        maquina =  document.getElementById("papel").value;
+    }else if(eleccionMaquina == document.getElementById("tijera").value){
+        maquina = document.getElementById("tijera").value;
+    }
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
-
+    
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
-
+    
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
-
+    
 }//FIN DE LA FUNCIÓN
