0f3bf0d8749cd78077ebb84531cb90ba09860378
rosasthomas <rosasthomas2@gmail.com>
2018-07-26 10:37:33 -0300

    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -3,12 +3,13 @@ function mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 switch(laHora){
-
+    case "7":
+    case "8":
+    case "9":
+    case "10":
+    case "11":
+            alert("Es de mañana");
 }
 //alert (laHora);
 	
-	
-
-
-
 }//FIN DE LA FUNCIÓN
